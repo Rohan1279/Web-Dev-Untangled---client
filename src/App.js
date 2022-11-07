e606d6f11a43e80d59c30b731420d7d95fa8c01c
Rohan1279 <muniruddinrohan@gmail.com>
2022-11-07 22:24:08 +0600

    shared page, layout and router created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { router } from "./routes/routes";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hi there! <br /> Firebase hosting is enabled now.
-      </h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/Shared/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className="h-6 bg-red-300 w-full">
+    
+  </div>;
+};
+
+export default Navbar;
