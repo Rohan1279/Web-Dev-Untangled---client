4e5472f0db0d4c32cbb9eb98980ad83998b0899f
Rohan1279 <muniruddinrohan@gmail.com>
2022-11-07 22:06:53 +0600

    firebase initialized and enabled hosting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>
+        Hi there! <br /> Firebase hosting is enabled now.
+      </h1>
+    </div>
+  );
 }
 
 export default App;
