f733337e8d18b82735da9eac24e06832c4ebdb9b
Rohan1279 <muniruddinrohan@gmail.com>
2022-11-08 15:40:29 +0600

    services card added in services page

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+import ServiceCard from "../Shared/ServiceCard";
 
 const Services = () => {
+  const services = useLoaderData();
+
   return (
     <div>
       <h2>Services Page</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 ">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service} />
+        ))}{" "}
+      </div>
     </div>
   );
 };
