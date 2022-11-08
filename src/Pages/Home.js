c1b35e808e82b43d807841d14c57dac9cfdef0a2
Rohan1279 <muniruddinrohan@gmail.com>
2022-11-08 08:03:02 +0600

    successfully fetched data from api in home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, [services]);
+
   return (
     <div>
       <h2>Home Page</h2>
+      <p>Found services: {services.length}</p>
     </div>
   );
 };
