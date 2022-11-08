fcb840c688f957ee50711136fb0444fe4c5ea858
Rohan1279 <muniruddinrohan@gmail.com>
2022-11-08 16:12:11 +0600

    Service section added in service detail page

--- src/Pages/ServiceDetail.js
+++ src/Pages/ServiceDetail.js
@@ -5,7 +5,23 @@ const ServiceDetail = () => {
   const serviceData = useLoaderData();
   const { _id, title, image, description, price, rating } = serviceData;
 
-  return <div></div>;
+  return (
+    <div>
+      <div className="card w-full bg-base-100 shadow-xl my-6">
+        <figure>
+          <img src={image} alt="" className="w-full" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-3xl">{title}</h2>
+          <p className="text-left">{description}</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-primary p-3">Price: ${price}</div>
+            <div className="badge badge-secondary p-3">Rating: {rating}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceDetail;
