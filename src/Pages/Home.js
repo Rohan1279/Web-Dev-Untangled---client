cdf4c0b7b8fec52e8a37b8372ba4114ba56b72eb
Rohan1279 <muniruddinrohan@gmail.com>
2022-11-10 23:26:26 +0600

    meaningful sections added in home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -46,9 +46,47 @@ const Home = () => {
             </div>
           </div>
         </div>
-      
       </section>
       {/* Hero section ends */}
+      <section className="dark:bg-gray-800 dark:text-gray-100 my-12">
+        <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+          <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+            <h1 className="text-5xl font-bold leading-none sm:text-6xl">
+              Learnings
+              <span className="dark:text-violet-400">Made</span>Easy write in
+              front
+            </h1>
+            <p className="mt-6 mb-8 text-lg sm:mb-12">
+              Discover your quests now!
+            </p>
+            <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+              <Link
+                rel="noopener noreferrer"
+                to={"/login"}
+                className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
+              >
+                Login
+              </Link>
+              <Link
+                rel="noopener noreferrer"
+                to={"/register"}
+                className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100"
+              >
+                Register
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+            <img
+              src="assets/svg/Business_SVG.svg"
+              alt=""
+              className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+            />
+          </div>
+        </div>
+      </section>
+
+      <h2 className="text-5xl my-5 text-white">Services I provide</h2>
       <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 ">
         {services.map((service) => (
           <ServiceCard key={service._id} service={service} />
@@ -57,6 +95,22 @@ const Home = () => {
       <button className="my-5 btn">
         <Link to={"/services"}>See all</Link>
       </button>
+      <section className="dark:bg-gray-800 dark:text-gray-100">
+        <div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
+          <h1 className="text-4xl font-bold leading-none sm:text-5xl my-5">
+            Become a member ?
+          </h1>
+          
+          <div className="flex flex-wrap justify-center">
+            <button className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">
+              Get started
+            </button>
+            <button className="px-8 py-3 m-2 text-lg border rounded dark:text-gray-50 dark:border-gray-700">
+              Learn more
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
