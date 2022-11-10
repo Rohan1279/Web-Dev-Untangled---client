ba789f057eaf05cc0c32052c0a48bbbb30607aab
Rohan1279 <muniruddinrohan@gmail.com>
2022-11-10 22:29:11 +0600

    implemented jwt token after registration

--- src/Authentication/Register.js
+++ src/Authentication/Register.js
@@ -22,9 +22,24 @@ const Register = () => {
     const password = form.password.value;
     createUser(email, password)
       .then((result) => {
-        console.log(result.user);
-        form.reset();
-        navigate(from, { replace: true });
+        const user = result.user;
+        const currentUser = {
+          email: user.email,
+        };
+        //get jwt token
+        fetch("http://localhost:5000/jwt", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(currentUser),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            localStorage.setItem("user-token", data.token);
+            navigate(from, { replace: true });
+          });
       })
       .catch((err) => console.log(err));
   };
